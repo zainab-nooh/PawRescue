9dcbd72d7cd357d023bdc908417b314f1fb8998b
Zainab-techie <zain3b.nooh@gmail.com>
2025-07-12 20:15:45 +0300

    Add all website files

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  contactNo:{
+    type: Number,
+    required:true
+  },
+  email:{
+    type:String,
+    required:true,
+    unique: true
+  },
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
