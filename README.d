e6722de759e8ed6583e5afafd2d3c6acaadcdfdb
zainab-nooh <zain3b.nooh@gmail.com>
2025-07-12 20:08:25 +0300

    Create README.d

---
+++ README.d
@@ -0,0 +1,2 @@
+# pawrescue
+Project Goal: PawRescue, a user-friendly website that bridges the gap between individuals offering pets for adoption, foster caregivers, small rescuers, or pet owners and the people seeking to adopt animals. The website will simplify the adoption process by enabling seamless profile creation, pet listings, and direct communication between parties. 
